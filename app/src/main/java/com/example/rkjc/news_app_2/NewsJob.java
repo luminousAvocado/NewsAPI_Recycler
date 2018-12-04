174d208671f23e592d275c4ecf821b8b234e0c72
luminousAvocado <jamesckang@gmail.com>
2018-12-04 08:40:29 -0800

    updates

--- app/src/main/java/com/example/rkjc/news_app_2/NewsJob.java
+++ app/src/main/java/com/example/rkjc/news_app_2/NewsJob.java
@@ -1,5 +1,48 @@
 package com.example.rkjc.news_app_2;
 
-public class NewsJob {
-    
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+import com.firebase.jobdispatcher.JobService;
+import com.firebase.jobdispatcher.JobParameters;
+
+public class NewsJob extends JobService {
+
+    static AsyncTask mBackgroundTask;
+
+    private static final String TAG = "NewsJob";
+
+    @Override
+    public boolean onStartJob(final JobParameters job) {
+        mBackgroundTask = new AsyncTask() {
+            @Override
+            protected void onPreExecute() {
+                Toast.makeText(NewsJob.this, "News updated!", Toast.LENGTH_SHORT).show();
+                super.onPreExecute();
+            }
+
+            @Override
+            protected Object doInBackground(Object[] params) {
+                Log.d(TAG, "doInBackground: Notification sync");
+                RefreshTask.executeTask(NewsJob.this, RefreshTask.ACTION_NOTIFICATION);
+                return null;
+            }
+
+            @Override
+            protected  void onPostExecute(Object o) {
+                jobFinished(job, false);
+                super.onPostExecute(o);
+            }
+        };
+
+        mBackgroundTask.execute();
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        if(mBackgroundTask != null) mBackgroundTask.cancel(false);
+
+        return true;
+    }
 }
---
+++ app/src/main/java/com/example/rkjc/news_app_2/NotificationUtils.java
@@ -0,0 +1,10 @@
+package com.example.rkjc.news_app_2;
+
+public class NotificationUtils {
+
+    private static final int NOTIFY_USER_NOTIFICATION_ID = 1;
+    private static final int ACTION_CANCEL_NOTIFICATION_ID = 14;
+    private static final int PENDING_INTENT_ID = 15;
+    private static final String NOTIFICATION_ID = "notification_channel";
+    private static final String TAG = "NotificationUtils";
+}
